82884cbf6fed6c576961d44295e9492ddccee110
akerpelm <akerpelm@u.rochester.edu>
2021-03-22 13:32:56 -0400

    Finish max_int_set, int_set

--- skeleton-3/lib/p01_int_set.rb
+++ skeleton-3/lib/p01_int_set.rb
@@ -1,20 +1,29 @@
 class MaxIntSet
+
+  attr_accessor :store
   def initialize(max)
-    
+    @store = Array.new(max, false)
+
   end
+# The MaxIntSet should raise an error if someone tries to insert, remove, or check inclusion of a number that is out of bounds.
 
   def insert(num)
+    raise 'Out of bounds' if !is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+   @store[num] = false if self.include?(num)
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, @store.length-1)
   end
 
   def validate!(num)
@@ -23,24 +32,30 @@ end
 
 
 class IntSet
+
+  attr_accessor :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
-   
+    return false if include?(num)
+    self[num] << num
+    num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
-  
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
+    self.store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -51,6 +66,7 @@ end
 
 class ResizingIntSet
   attr_reader :count
+  attr_accessor :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -58,17 +74,21 @@ class ResizingIntSet
   end
 
   def insert(num)
+    self[num] << num if !include?(num)
   end
 
   def remove(num)
+    self[num].pop
   end
 
   def include?(num)
+     self[num].include?(num)
   end
 
   private
 
   def [](num)
+    self.store[num % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
